b1d4d996aa69210453e5cf9995a18cefe72caaac
HADO564 <70658014+HADO564@users.noreply.github.com>
2022-05-15 14:51:38 +0500

    Creation of AP successful. Will Attempt request sending

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,7 @@
 #define AP_PASS "HADOboi21"
 WiFiClient Wifi1;
 
-
-
+void PrintStations();
 void setup() {
   Serial.begin(SERIAL_SPEED);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -21,5 +20,39 @@ void setup() {
 }
 
 void loop() {
-  
+  PrintStations();
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(2000);
+}
+
+
+
+void PrintStations(){
+  wifi_sta_list_t stationList;
+ 
+  esp_wifi_ap_get_sta_list(&stationList);  
+ 
+  Serial.print("Number of connected stations: ");
+  Serial.println(stationList.num);
+ 
+  for(int i = 0; i < stationList.num; i++) {
+ 
+    wifi_sta_info_t station = stationList.sta[i];
+ 
+    for(int j = 0; j< 6; j++){
+      char str[3];
+ 
+      sprintf(str, "%02x", (int)station.mac[j]);
+      Serial.print(str);
+ 
+      if(j<5){
+        Serial.print(":");
+      }
+    }
+    Serial.println();
+  }
+ 
+  Serial.println("-----------------");
 }
