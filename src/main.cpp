8085e64618369b16c77e6a0767d56c64ef42feef
HADO564 <70658014+HADO564@users.noreply.github.com>
2022-05-25 19:27:53 +0500

    Added support for handling JSON files

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include "WiFi.h"
 #include "esp_wifi.h"
+#include "HTTPClient.h"
+#include <ArduinoJson.h>//for servicing json files
 #define LED_BUILTIN 2
-
+#define GPIO 15
 #define SERIAL_SPEED 115200
 #define AP_SSID "HADO_ESP32"
 #define AP_PASS "HADOboi21"
@@ -13,21 +15,28 @@ void setup() {
   Serial.begin(SERIAL_SPEED);
   pinMode(LED_BUILTIN, OUTPUT);
   WiFi.softAP(AP_SSID,AP_PASS);
-
+  pinMode(GPIO,OUTPUT);
   Serial.println();
   Serial.print("IP address: ");
   Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
-  PrintStations();
+  // PrintStations();
   digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(GPIO,LOW);
   delay(1000);
   digitalWrite(LED_BUILTIN,LOW);
+  digitalWrite(GPIO,HIGH);
   delay(2000);
+  delay(3000);
 }
 
 
+void Authenticate(wifi_sta_info_t station)
+{
+
+}
 
 void PrintStations(){
   wifi_sta_list_t stationList;
